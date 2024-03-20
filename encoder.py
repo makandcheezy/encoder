811206a556613df98240e7935ae667e35719de95
Han Nguyen <daniylnguyen776@gmail.com>
2024-03-20 12:26:52 -0400

    Added decode function

--- encoder.py
+++ encoder.py
@@ -1,3 +1,4 @@
+# Adam Makled
 def encode(password):
     if not password.isdigit() or len(password) != 8:
         return "Invalid password. Please enter an 8-digit password containing only numbers."
@@ -5,20 +6,31 @@ def encode(password):
     encoded_password = ''.join(str((int(char) + 3) % 10) for char in password)
     return encoded_password
 
+# Daniyl Nguyen
+def decode(encoded):
+    original = ""
+    for char in encoded:
+        original += str(abs((int(char) + 7) % 10))
+    return original
 
 def main():
+    password_to_encode = ""
+    encoded_password = ""
     while True:
         print("\nPassword Encoder")
         print("1. Encode a password")
-        print("2. Exit")
+        print("2. Decode a password")
+        print("3. Exit")
 
-        choice = input("Enter your choice (1 or 2): ")
+        choice = input("Enter your choice (1, 2, or 3): ")
 
         if choice == "1":
             password_to_encode = input("Enter an 8-digit password to encode: ")
             encoded_password = encode(password_to_encode)
             print(f"Encoded password: {encoded_password}")
         elif choice == "2":
+            print("The encoded password is " + str(encoded_password) + ", and the original password is " + str(decode(encoded_password)))
+        elif choice == "3":
             print("Exiting program.")
             break
         else:
